3b76329a45b5ac4bf8e8dfe50b7e2553134e63f3
JLDeb <99515799+JLDeb@users.noreply.github.com>
2025-07-23 19:12:45 -0300

    Add files via upload

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -69,7 +69,7 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
-Browser.site = 'https://boilerplate-mochachai.rocks';
+Browser.site = 'https://boilerplate-mochachai-tuow.onrender.com/';
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
   const browser = new Browser();
