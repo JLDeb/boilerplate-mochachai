cb497c98d7d0e3b6b73488d5e0d9e40c728df395
JLDeb <99515799+JLDeb@users.noreply.github.com>
2025-07-23 20:29:22 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,7 +70,7 @@ suite('Functional Tests', function () {
 
 const Browser = require('zombie');
 
-Browser.site = 'http://0.0.0.0:3000'; 
+Browser.site = 'https://boilerplate-mochachai-tuow.onrender.com'; 
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
